ca06340bc4bf08f1538093e6c042e23ef6086c2f
Thomas VINET <thomas.vinet3@gmail.com>
2025-10-30 21:15:07 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <input id="app-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+
+    </div>
+    <div class="drawer-side">
+      <label for="app-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+      <div class="flex menu bg-base-200 min-h-full w-80 p-4">
+      <span class="text-3xl"> Thomas VINET </span>
+      <hr>
+      <ul>
+        <li>
+          <div class="flex">
+            <font-awesome-icon icon="fa-solid fa-house"/>
+            <RouterLink to="/">Home</RouterLink>
+          </div>
+        </li>
+        <li>
+          <div class="flex">
+            <font-awesome-icon icon="fa-solid fa-house"/>
+            <RouterLink to="/">Publications</RouterLink>
+          </div>
+        </li>
+        <li>
+          <div class="flex">
+            <font-awesome-icon icon="fa-solid fa-house"/>
+            <RouterLink to="/">Teaching</RouterLink>
+          </div>
+        </li>
+        <li>
+          <div class="flex">
+            <font-awesome-icon icon="fa-solid fa-globe"/>
+            <RouterLink to="/">Contact</RouterLink>
+          </div>
+        </li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
+const app = createApp(App);
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router);
+
+app.mount("#app");
