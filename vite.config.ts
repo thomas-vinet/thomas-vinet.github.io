afe26f8a0d388e176de2c9965e80630a86a9eedd
Thomas VINET <thomas.vinet3@gmail.com>
2025-10-31 21:57:36 +0100

    mime type

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [tailwindcss(), vue(), vueDevTools()],
+  base: '/thomas-vinet',
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
